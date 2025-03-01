ed201adb612d82eb53267bf29bd42bcb0c8bc2bf
Jon Salud <salud.jon@gmail.com>
2025-03-01 21:11:33 +1300

    Enhances e2e test framework and main page navigation
    Introduces fixture for main page in e2e tests
    Adds utility function to locate menu links
    Simplifies main page load verification logic
    Implements navigation method in main page class
    Updates test to use new navigation method and fixture

--- e2e/tests/test1.spec.ts
+++ e2e/tests/test1.spec.ts
@@ -1,32 +1,32 @@
 // e2e\tests\test1.spec.ts
 
-import test from "@playwright/test";
+import test from "../fixtures/pages";
 import { MainPage } from "../pages/mainPage";
 
-test.beforeEach(async ({ page }) => {
-    const mainPage = new MainPage(page);
-    await mainPage.openMainPage();    
+test.beforeEach(async ({ mainPage, page }) => {
+    await mainPage.goToMainPage();
 });
 
 test.afterEach(async ({ page }, testInfo) => {
     let testStatus = testInfo.status?.toUpperCase();
     if (testStatus == "PASSED") {
-        console.log(`   \u001b[32m< PASSED >\u001b[0m ${testInfo.title}`); 
+        console.log(`   \u001b[32m< PASSED >\u001b[0m ${testInfo.title}`);
     } else {
-        console.log(`   \u001b[31m< FAILED >\u001b[0m ${testInfo.title}`); 
+        console.log(`   \u001b[31m< FAILED >\u001b[0m ${testInfo.title}`);
     }
     await page.close();
 });
 
-test('Validate Error page', async ({ page }) => {
+test("Validate Error page", async ({ page, mainPage }) => {
     await test.step("Step 1 - Go to Contact Page", async () => {
-        // From the home page go to contact page
-    })
+        await mainPage.navigateToPage("Contact");
+        // await contactPage.ensureContactPageIsVisible();
+    });
 
     await test.step("Step 2 - Click submit button", async () => {
         // Click submit button
-    })
-    
+    });
+
     await test.step("Step 3 - Verify error messages", async () => {
         // Verify error messages
     });
@@ -38,6 +38,4 @@ test('Validate Error page', async ({ page }) => {
     await test.step("Step 5 - Validate errors are gone", async () => {
         // Validate errors are gone
     });
-    
-})
-
+});
