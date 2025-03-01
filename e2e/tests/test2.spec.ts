2d08d18ecb2d659c38c10897a845e691b428530c
Jon Salud <salud.jon@gmail.com>
2025-03-02 00:18:24 +1300

    Adds verification for successful submission and refines test flow
    Introduces method to verify success message post form submission on the contact page.
    Enhances test to validate submission process five times with detailed steps.
    Updates configuration to adjust timeouts and enable screenshots on failure.
    
    Fixes #TC2

--- e2e/tests/test2.spec.ts
+++ e2e/tests/test2.spec.ts
@@ -17,23 +17,30 @@ test.afterEach(async ({ page }, testInfo) => {
     await page.close();
 });
 
-test("Validate Successful submission of message on the Contact Page", { tag: "@RegressionTest" }, async ({ page, contactPage }) => {
+test("Validate Successful submission of message on the Contact Page 5 times", { tag: "@RegressionTest" }, async ({ page, contactPage }) => {
+    const forename = "Johnny Bravo";
+    const email = "abc@123.com";
+    const message = "This is a test message";
+
     for (let i = 0; i < 5; i++) {
-        await test.step("Step 1 - Go to Contact Page", async () => {
+        await test.step(`Step ${i + 1}-1 - Go to Contact Page`, async () => {
             await navigateToPage(page, "Contact");
             await contactPage.ensureContactPageIsVisible();
         });
 
-        await test.step("Step 2 - Populate mandatory fields", async () => {
-            // Populate mandatory fields
+        await test.step(`Step ${i + 1}-2 - Populate mandatory fields`, async () => {
+            await contactPage.setForename(forename);
+            await contactPage.setEmail(email);
+            await contactPage.setMessage(message);
         });
 
-        await test.step("Step 3 - Click submit button", async () => {
-            // Click submit button
+        await test.step(`Step ${i + 1}-3 - Click submit button`, async () => {
+            await contactPage.clickSubmitButton();
         });
 
-        await test.step("Step 4 - Validate successful submission message", async () => {
-            // Validate successful submission message
+        await test.step(`Step ${i + 1}-4 - Validate successful submission message`, async () => {
+            await contactPage.verifySuccessfulSubmissionMessage(forename);
+            await navigateToPage(page, "Home");
         });
     }
 });
