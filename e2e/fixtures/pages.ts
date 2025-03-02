955c1d4d2ef4e3708465a4ba576ec2af6ca3a133
Jon Salud <salud.jon@gmail.com>
2025-03-02 18:37:34 +1300

    Add ShopPage fixture to extend test capabilities

--- e2e/fixtures/pages.ts
+++ e2e/fixtures/pages.ts
@@ -1,16 +1,21 @@
 import { test as baseTests } from "@playwright/test";
 import { MainPage } from "../pages/mainPage";
 import { ContactPage } from "../pages/contactPage";
+import { ShopPage } from "../pages/shopPage";
 
 const test = baseTests.extend<{
     mainPage: MainPage;
     contactPage: ContactPage;
+    shopPage: ShopPage;
 }>({
     mainPage: async ({ page }, use) => {
         await use(new MainPage(page));
     },
     contactPage: async ({ page }, use) => {
         await use(new ContactPage(page));
+    },
+    shopPage: async ({ page }, use) => {
+        await use(new ShopPage(page));
     },
 });
 
